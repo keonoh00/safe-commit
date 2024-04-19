bc9f2faf8f0565ae965efbc9034be411ecc9811e
keonoh00 <keonoh00@gmail.com>
2024-04-19 20:54:14 +0900

    Add Drawer with hamburger button

---
+++ web/src/components/IDrawer/IDrawer.tsx
@@ -0,0 +1,58 @@
+import { HamburgerIcon } from "@chakra-ui/icons";
+import {
+  Button,
+  Drawer,
+  DrawerBody,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  useDisclosure,
+} from "@chakra-ui/react";
+import React from "react";
+
+export interface IDrawerProps {
+  children?: React.ReactNode;
+  title?: string;
+}
+
+const IDrawer = ({ title, children }: IDrawerProps) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const btnRef = React.useRef();
+
+  return (
+    <>
+      <Button
+        ref={btnRef.current}
+        colorScheme={"whiteAlphaka"}
+        onClick={onOpen}
+      >
+        <HamburgerIcon w={8} h={8} color={"black"} />
+      </Button>
+      <Drawer
+        isOpen={isOpen}
+        placement="right"
+        onClose={onClose}
+        finalFocusRef={btnRef.current}
+      >
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton />
+          {title ? <DrawerHeader>{title}</DrawerHeader> : null}
+
+          <DrawerBody>{children}</DrawerBody>
+
+          <DrawerFooter>
+            <Button variant="outline" mr={3} onClick={onClose}>
+              Cancel
+            </Button>
+            <Button colorScheme="blue">Save</Button>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+    </>
+  );
+};
+
+export default IDrawer;
--- web/src/components/IHeader/IHeader.tsx
+++ web/src/components/IHeader/IHeader.tsx
@@ -1,13 +1,15 @@
-import { Box, Button, chakra, Flex, Image } from "@chakra-ui/react";
+import { Flex, Image } from "@chakra-ui/react";
 
-const RouteButton = chakra(Button, {
-  baseStyle: {
-    color: "black",
-    marginRight: "5px",
-  },
-});
+import { useNavigate } from "react-router-dom";
+import IDrawer from "../IDrawer/IDrawer";
 
 export default function IHeader() {
+  const navigate = useNavigate();
+
+  const pushToHome = () => {
+    navigate("/");
+  };
+
   return (
     <Flex
       paddingBlock={6}
@@ -16,21 +18,9 @@ export default function IHeader() {
       alignItems={"center"}
       borderColor={"gray.200"}
     >
-      <Image
-        // src={AssetSelector.logo}
-        alt="logo"
-        aspectRatio={6 / 1}
-        // onClick={pushToHome}
-      />
+      <Image alt="logo" aspectRatio={6 / 1} onClick={pushToHome} />
 
-      <Box>
-        <RouteButton colorScheme={"whiteAlpaka"} onClick={() => {}}>
-          Home
-        </RouteButton>
-        <RouteButton colorScheme={"whiteAlpaka"} onClick={() => {}}>
-          Demo
-        </RouteButton>
-      </Box>
+      <IDrawer />
     </Flex>
   );
 }
